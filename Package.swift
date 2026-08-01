ca3807879680ab77f37755190debeb6f413f3e8f
JC Grubbs <grubbs.jc@gmail.com>
2026-08-01 08:42:27 -0500

    T-tj6hor cover persisted scalar encoding

--- Package.swift
+++ Package.swift
@@ -3,18 +3,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "TendCore",
-    platforms: [
-        .iOS(.v26),
-        .macOS(.v26),
-    ],
-    products: [
-        .library(name: "TendCore", targets: ["TendCore"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(name: "TendCore"),
-        .testTarget(name: "TendCoreTests", dependencies: ["TendCore"]),
-    ],
-    swiftLanguageModes: [.v6]
+  name: "TendCore",
+  platforms: [
+    .iOS(.v26),
+    .macOS(.v26),
+  ],
+  products: [
+    .library(name: "TendCore", targets: ["TendCore"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(name: "TendCore"),
+    .testTarget(name: "TendCoreTests", dependencies: ["TendCore"]),
+  ],
+  swiftLanguageModes: [.v6]
 )
